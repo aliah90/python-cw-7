459b86b0975e8867a4e931c40eeb6cf4d9ec6725
aliah90 <118757472+aliah90@users.noreply.github.com>
2022-12-14 20:05:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 
 # write your code here
+class Person:
+    #name = 'alia'
+    #age = 33
+
+    def __init__(self,name,age):
+        self.name= name
+        self.age= age
+
+    def is_adult(self):
+        if self.age > 18:
+             print("You have too much responsibilities")
+        elif self.age < 18:
+            print("Lucky you")
+    def __str__(self):
+        return f"My name is {self.name} and I am {self.age} years old "
+first_person = Person('alia',33)
+print(first_person.name , first_person.age)
+first_person.is_adult()
+print(first_person)
+    
+
